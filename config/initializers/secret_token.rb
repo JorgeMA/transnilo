77c9e658002d5cdbed9f562b7491f402d0b550ea
Jorge Moisés Assis <jorgemoisesassis@gmail.com>
2013-02-26 00:08:56 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Transnilo::Application.config.secret_token = '4598fe9f74bad8bcef286fe4e3f89c9cda42072333b0914df57a552ed7aaa32bbb8785deef122e5335ee3bdb830435b4741d7a222ac7636389244933a11f2530'
